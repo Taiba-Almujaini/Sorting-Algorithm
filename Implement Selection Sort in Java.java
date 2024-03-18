619eb043110860ac3be5520946a29e63efd5aa71
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-03-18 09:30:28 +0400

    Implement Selection Sort in Java.java

--- Implement Selection Sort in Java.java
+++ Implement Selection Sort in Java.java
@@ -1,15 +1,25 @@
+import java.util.ArrayList;
+
 public class SortingAlgoritm {
     public static void main(String[] args) {
-        Integer[] arrayToSort = {324, 123, 556, 454, 121};
+        ArrayList<Integer> arrayToSort = new ArrayList<>();
+        arrayToSort.add(324);
+        arrayToSort.add(123);
+        arrayToSort.add(556);
+        arrayToSort.add(454);
+        arrayToSort.add(121);
+
         selectionSort(arrayToSort);
+
+        // Print the sorted ArrayList
         System.out.println("Sorted array:");
-        for (Integer i = 0; i < arrayToSort.length; i++) { //print sorted array
-            System.out.print(arrayToSort[i] + " ");
+        for (Integer number : arrayToSort) {
+            System.out.print(number + " ");
         }
     }
 
-    public static void selectionSort(Integer[] sortedArray) {
-        Integer sizeOfArrayToSort = sortedArray.length;
+    public static void selectionSort(ArrayList<Integer> sortedArray) {
+        Integer sizeOfArrayToSort = sortedArray.size();
 
         // Loop  from the first to the second-to-last element
         for (Integer i = 0; i < sizeOfArrayToSort ; i++) {
@@ -19,15 +29,15 @@ public class SortingAlgoritm {
             // Check the rest of the array for something smaller
             for (Integer j = i + 1; j < sizeOfArrayToSort; j++) {
                 // If a smaller element is found, update minNumberIndex
-                if (sortedArray[j] < sortedArray[minNumberIndex]) {
+                if (sortedArray.get(j) < sortedArray.get(minNumberIndex)) {
                     minNumberIndex = j;
                 }
             }
 
             // Swap the smallest found element with the first unsorted position
-            Integer tempSwapVariable = sortedArray[minNumberIndex];
-            sortedArray[minNumberIndex] = sortedArray[i];
-            sortedArray[i] = tempSwapVariable;
+            Integer tempSwapVariable = sortedArray.get(minNumberIndex);
+            sortedArray.set(minNumberIndex, sortedArray.get(i));
+            sortedArray.set(i, tempSwapVariable);
         }
     }
 }
